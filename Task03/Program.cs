6eccd322331a03aa933efdcc2225251d6b76f471
Nikita <kindpavlov@yandex.ru>
2023-03-10 10:58:47 +0300

    Добавили задачу Task09.Решили несколькими способами

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -6,35 +6,35 @@
 Console.WriteLine("Введите номер дня недели:");
 
 string day = Console.ReadLine();
-if(day == "1")
+if (day == "1")
 {
   Console.WriteLine("Понедельник");
 }
-else if(day == "2")
+else if (day == "2")
 {
   Console.WriteLine("Вторник");
 }
-else if(day == "3")
+else if (day == "3")
 {
   Console.WriteLine("Среда");
 }
-else if(day == "4")
+else if (day == "4")
 {
   Console.WriteLine("Четверг");
 }
-else if(day == "5")
+else if (day == "5")
 {
   Console.WriteLine("Пятница");
 }
-else if(day == "6")
+else if (day == "6")
 {
   Console.WriteLine("Суббота");
 }
-else if(day == "7")
+else if (day == "7")
 {
   Console.WriteLine("Воскресенье");
 }
-else 
+else
 {
-   Console.WriteLine("Ты похоже с другой планеты, т.к. у нас всего семь дней в неделе :))");
+  Console.WriteLine("Ты похоже с другой планеты, т.к. у нас всего семь дней в неделе :))");
 }
