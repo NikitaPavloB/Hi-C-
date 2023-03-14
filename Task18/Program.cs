40e5823c3e863cb809543bf4cf8fb10b0bf2f27a
Nikita <kindpavlov@yandex.ru>
2023-03-14 11:17:30 +0300

    Исправил ошибочку  Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -11,9 +11,9 @@ Console.WriteLine(result);
 
 string GetCoordinate(int x)
 {
-  if (x == 1) return "Диапазон 1 четверти: x > 0 && y > 0";
-  if (x == 2) return "Диапазон 2 четверти: x < 0 && y > 0";
-  if (x == 3) return "Диапазон 3 четверти: x < 0 && y < 0";
-  if (x == 4) return "Диапазон 4 четверти: x > 0 && y < 0"; 
+  if (x == 1) return "Диапазон 1 четверти: x > 0, y > 0";
+  if (x == 2) return "Диапазон 2 четверти: x < 0, y > 0";
+  if (x == 3) return "Диапазон 3 четверти: x < 0, y < 0";
+  if (x == 4) return "Диапазон 4 четверти: x > 0, y < 0"; 
   return "Некорректный номер четверти";
 }
