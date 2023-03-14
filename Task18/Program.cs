1efd657757d4ea211f7f21f50ad8109e43ada4c0
Nikita <kindpavlov@yandex.ru>
2023-03-14 11:25:45 +0300

    В этом коммите еще один вариант решения задачи  Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -4,16 +4,15 @@
 
 Console.Clear();
 Console.WriteLine("Введите номер четверти:");
-int quater = Convert.ToInt32(Console.ReadLine());
-
-string result = GetCoordinate(quater);
+string ? quater = Console.ReadLine();
+string ? result = GetCoordinate(quater);
 Console.WriteLine(result);
 
-string GetCoordinate(int x)
+string GetCoordinate(string x)
 {
-  if (x == 1) return "Диапазон 1 четверти: x > 0, y > 0";
-  if (x == 2) return "Диапазон 2 четверти: x < 0, y > 0";
-  if (x == 3) return "Диапазон 3 четверти: x < 0, y < 0";
-  if (x == 4) return "Диапазон 4 четверти: x > 0, y < 0"; 
+  if (x == "1") return "Диапазон 1 четверти: x > 0, y > 0";
+  if (x == "2") return "Диапазон 2 четверти: x < 0, y > 0";
+  if (x == "3") return "Диапазон 3 четверти: x < 0, y < 0";
+  if (x == "4") return "Диапазон 4 четверти: x > 0, y < 0";
   return "Некорректный номер четверти";
 }
